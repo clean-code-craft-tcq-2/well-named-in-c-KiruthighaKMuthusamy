e0f2d381ec10b4714835e192255fe4a717276be2
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 19:10:17 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
+#include "ColorPair.h"
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+#define MAX_COLORPAIR_NAME_CHARS  16
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
@@ -9,16 +15,17 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+int numberOfMajorColors;
+int numberOfMinorColors;
 
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
+extern void testPairToNumber(    enum MajorColor major,    enum MinorColor minor,    int expectedPairNumber);
+extern int GetPairNumberFromColor(const ColorPair* colorPair) ;
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
+extern ColorPair GetColorFromPairNumber(int pairNumber);
+extern void testNumberToPair(int pairNumber,    enum MajorColor expectedMajor,    enum MinorColor expectedMinor);
+
