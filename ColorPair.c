1ab152587f53a8b17c62160d77380359f64eee8e
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 19:59:00 +0530

    Update ColorPair.c

--- ColorPair.c
+++ ColorPair.c
@@ -16,9 +16,10 @@ int numberOfMinorColors =
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+	
+    PrintManual();
 
     return 0;
 }
