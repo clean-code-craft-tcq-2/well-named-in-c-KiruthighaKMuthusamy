6496a07602560877357de8fefb3cc222389fab08
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 20:12:09 +0530

    Update PrintManual.c

--- PrintManual.c
+++ PrintManual.c
@@ -14,7 +14,8 @@ void PrintManual()
 	for (int ColorNumber= 1 ; ColorNumber <= TotalColorPairNum ; ColorNumber++)
 	{
 	  ColorPair colorPair = GetColorFromPairNumber(ColorNumber);
-	  char MajorColorName[MAX_COLORPAIR_NAME_CHARS/2], MinorColorName[MAX_COLORPAIR_NAME_CHARS/2];  
+	  char MajorColorName[MAX_COLORPAIR_NAME_CHARS/2];
+	  char MinorColorName[MAX_COLORPAIR_NAME_CHARS/2];  
 	  ColorNamesToString(&colorPair,MajorColorName,MinorColorName);
 	  printf("Pair Number: %d Major and Minor Color Names: %s ,%s\n", ColorNumber, MajorColorName, MinorColorName);
 	  
