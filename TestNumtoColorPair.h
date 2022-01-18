b4c9ca42d1872f16b1d8ce06ad938cddc3078fd4
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 19:08:21 +0530

    Delete TestNumtoColorPair.h

--- TestNumtoColorPair.h
+++
@@ -1,5 +0,0 @@
-#include "ColorPair.h"
-
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
-extern ColorPair GetColorFromPairNumber(int pairNumber);
-extern void testNumberToPair(int pairNumber,    enum MajorColor expectedMajor,    enum MinorColor expectedMinor);
