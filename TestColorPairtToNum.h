69690c69e274d583474056bb87e41b837616d56d
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 13:59:55 +0530

    Update TestColorPairtToNum.h

--- TestColorPairtToNum.h
+++ TestColorPairtToNum.h
@@ -1,4 +1,4 @@
-#include <ColorPair.h>
+#include "ColorPair.h"
 
 extern void testPairToNumber(    enum MajorColor major,    enum MinorColor minor,    int expectedPairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair) ;
