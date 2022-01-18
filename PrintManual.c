c2407dc94d672e59c0711b280414b1ca0d7dd379
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 19:58:25 +0530

    Update PrintManual.c

--- PrintManual.c
+++ PrintManual.c
@@ -1,6 +1,6 @@
 #include "ColorPair.h"
 
-void ColorNamesToString(const ColorPair* colorPair, char* Majorbuffer, char* Minorbuffer)
+void ColorNamesToString(const ColorPair* colorPair, char* MajorColorbuffer, char* MajorColorbuffer)
  {
     sprintf(MajorColorbuffer, "%s",
         MajorColorNames[colorPair->majorColor]);
@@ -13,11 +13,11 @@ void PrintManual()
  {
   int TotalColorPairNum = numberOfMajorColors * numberOfMinorColors;
   printf("~~~~~~~~~~~~~COLOR CODE MANUAL~~~~~~~~~~~~~~~\n");
-	for (int ColorNumber= 1 ; i<=TotalColorPairNum ; ColorNumber++)
+	for (int ColorNumber= 1 ; ColorNumber <= TotalColorPairNum ; ColorNumber++)
 	{
 	  ColorPair colorPair = GetColorFromPairNumber(ColorNumber);
 	  char MajorColorName[MAX_COLORPAIR_NAME_CHARS/2], MinorColorName[MAX_COLORPAIR_NAME_CHARS/2];  
-	  ColorNamesToString(colorPair,MajorColorName,MinorColorName);
+	  ColorNamesToString(&colorPair,MajorColorName,MinorColorName);
 	  printf("Pair Number: %d Major and Minor Color Names: %s ,%s\n", ColorNumber, MajorColorName, MinorColorName);
 	  
 	}
