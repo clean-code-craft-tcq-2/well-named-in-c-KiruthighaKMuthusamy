e3fbbf29c52fd37b88485b86e2831a0875740950
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-01-18 15:11:59 +0530

    Add files via upload

---
+++ TestColorPairToNum.h
@@ -0,0 +1,4 @@
+#include "ColorPair.h"
+
+extern void testPairToNumber(    enum MajorColor major,    enum MinorColor minor,    int expectedPairNumber);
+extern int GetPairNumberFromColor(const ColorPair* colorPair) ;
